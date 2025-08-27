a89f61d8e6bdc8966ab83467a0659c4c92d31f05
johwick546-cloud <johwick546@gmail.com>
2025-08-27 13:54:29 -0700

    Delete assets directory

--- assets/js/portal-auth.js
+++
@@ -1,8 +0,0 @@
-function unlockPortal(portalId, correctPassword) {
-  const input = prompt("Enter password:");
-  if (input === correctPassword) {
-    document.getElementById(portalId).style.display = "block";
-  } else {
-    alert("Incorrect password.");
-  }
-}
